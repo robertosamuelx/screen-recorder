7373d163c63ff89c600ccd9089c18a6b94ad8803
robertosamuelx <roberto-samuel1@hotmail.com>
2020-05-15 00:37:20 -0300

    release-candidate-1.0.0

---
+++ README.md
@@ -0,0 +1,23 @@
+# My Screen Recorder #
+
+- ### *Objetivo*: ###
+> - Criar uma aplicação para gravação de telas
+
+- ### *Como funciona*: ###
+> - Ao abrir a aplicação o usuário tem a opção de escolher uma tela a qual deseja
+gravar, ao selecionar a tela ele pode pressionar o botão de Iniciar para começar o processo de gravação. No centro da aplicação será exibido em tempo real a tela gravada e abaixo será mostrado com quanto tempo (em segundos) está o video. Ao clicar em parar, uma tela de salvamento será aberta para o usuário selecionar onde quer salvar o vídeo gravado.
+
+- ### *Requisitos*: ###
+> - Node v12.16.1 ou superior
+> - Yarn 1.22.4 ou superior (pode ser substituído por NPM 6.13.4 ou superior)
+
+- ### *Como usar*: ###
+> - git clone https://github.com/robertosamuelx/screen-recorder.git
+> - yarn add (ou npm i)
+> - yarn start (ou npm start)
+
+---
+
+- ### **APRESENTAÇÃO** ###
+![Main](prints/main.png)
+![Recording](prints/recording.png)
---
+++ src/render.js
@@ -0,0 +1,106 @@
+const videoElement = document.querySelector('video');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+const { desktopCapturer , remote} = require('electron');
+const { Menu } = remote;
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const recorderTime = document.getElementById('recorderTime');
+let time;
+stopBtn.disabled = true;
+videoSelectBtn.onclick = e => getVideoSources();
+
+async function getVideoSources(){
+    console.log('click');
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window','screen']
+    });
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            }
+        })
+    );
+
+    videoOptionsMenu.popup();
+}
+
+let mediaRecorder;
+const recordedChunks = [];
+
+
+async function selectSource(source){
+    videoSelectBtn.innerText = source.name;
+
+    const constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+    };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+    videoElement.srcObject = stream;
+    videoElement.play();
+
+    const options = { mimeType: 'video/webm; codecs=vp9'};
+    mediaRecorder = new MediaRecorder(stream,options);
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+}
+
+let interval;
+
+startBtn.onclick = e => {
+  mediaRecorder.start();
+  startBtn.innerText = 'Gravando...';
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  time = Date.now();
+    interval = setInterval(()=>{
+        recorderTime.innerText = `Tempo de gravação: ${ parseInt((Date.now() - time) / 1000)} segundos`;
+    },1000);
+};
+
+
+
+stopBtn.onclick = e => {
+  mediaRecorder.stop();
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  startBtn.innerText = 'Iniciar';
+  recorderTime.innerText = 'Tempo de gravação: 0 segundos';
+  clearInterval(interval);
+};
+
+
+function handleDataAvailable(e){
+    console.log('video data available');
+    recordedChunks.push(e.data);
+}
+
+const { dialog } = remote;
+const { writeFile } = require('fs');
+async function handleStop(e){
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm; codecs=vp9'
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath } = await dialog.showSaveDialog({
+        buttonLabel: 'Salvar video',
+        defaultPath: `video-${Date.now()}.webm`
+    });
+
+    console.log(filePath);
+
+    writeFile(filePath,buffer,() => console.log('video salvo'));
+}
